8e690f72824b474efcb41e8291f3f1f9d16c87e5
NEKOYASAN <hello@nekoyasan.me>
2025-04-16 22:45:37 +0900

    attach Amazon Location Service Style

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,16 @@
 import { MapView } from "@maplibre/maplibre-react-native";
 
+if (!process.env.EXPO_PUBLIC_ALS_API_KEY) {
+  throw new Error("EXPO_PUBLIC_ALS_API_KEY is not set");
+}
+if (!process.env.EXPO_PUBLIC_ALS_REGION) {
+  throw new Error("EXPO_PUBLIC_ALS_REGION is not set");
+}
+
+const mapStyleUrl = `https://maps.geo.${process.env.EXPO_PUBLIC_ALS_REGION}.amazonaws.com/v2/styles/Standard/descriptor?key=${process.env.EXPO_PUBLIC_ALS_API_KEY}`
+
 export default function Index() {
   return (
-    <MapView style={{ flex: 1 }} />
+    <MapView style={{ flex: 1 }} mapStyle={mapStyleUrl} />
   );
 }
